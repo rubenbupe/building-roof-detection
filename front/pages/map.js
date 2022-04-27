0ec61f03a512bfb912727ae9b874aca721d25ac0
JavsJavs <45043131+JavsJavs@users.noreply.github.com>
2022-04-27 19:19:08 +0200

    fix: fixed the toggles resetting when changing between the Map and PhotoUpload pages

--- front/pages/map.js
+++ front/pages/map.js
@@ -1,7 +1,7 @@
 import Header from "../src/components/Header/Header"
 import dynamic from 'next/dynamic'
 import Footer from '../src/components/Footer/Footer';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 function MapScreen() {
     const [segmentationSwitch, setSegmentationSwitch] = useState(false);
@@ -9,6 +9,28 @@ function MapScreen() {
     const Map = dynamic(() => import('../src/components/Map/Map'), // replace '@components/map' with your component's location
         {ssr: false} // This line is important. It's what prevents server-side render
     )
+
+    useEffect(() => {
+        if (localStorage.getItem('segmentationSwitch')) {
+            setSegmentationSwitch((localStorage.getItem('segmentationSwitch') === 'true'));
+        } else {
+            localStorage.setItem('segmentationSwitch', segmentationSwitch.toString());
+        }
+
+        if (localStorage.getItem('serverSwitch')) {
+            setServerSwitch((localStorage.getItem('serverSwitch') === 'true'));
+        } else {
+            localStorage.setItem('serverSwitch', serverSwitch.toString());
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('segmentationSwitch', segmentationSwitch.toString());
+    }, [segmentationSwitch]);
+
+    useEffect(() => {
+        localStorage.setItem('serverSwitch', serverSwitch.toString());
+    }, [serverSwitch]);
 
     return (
         <div>
--- front/pages/photo.js
+++ front/pages/photo.js
@@ -1,11 +1,34 @@
 import PhotoUpload from "../src/components/PhotoUpload/PhotoUpload"
 import Header from "../src/components/Header/Header"
 import Footer from '../src/components/Footer/Footer';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 function Photo() {
     const [segmentationSwitch, setSegmentationSwitch] = useState(false);
     const [serverSwitch, setServerSwitch] = useState(false);
+
+    useEffect(() => {
+        if (localStorage.getItem('segmentationSwitch')) {
+            setSegmentationSwitch((localStorage.getItem('segmentationSwitch') === 'true'));
+        } else {
+            localStorage.setItem('segmentationSwitch', segmentationSwitch.toString());
+        }
+
+        if (localStorage.getItem('serverSwitch')) {
+            setServerSwitch((localStorage.getItem('serverSwitch') === 'true'));
+        } else {
+            localStorage.setItem('serverSwitch', serverSwitch.toString());
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('segmentationSwitch', segmentationSwitch.toString());
+    }, [segmentationSwitch]);
+
+    useEffect(() => {
+        localStorage.setItem('serverSwitch', serverSwitch.toString());
+    }, [serverSwitch]);
+
     return (
         <div>
             <script src="https://docs.opencv.org/master/opencv.js" type="text/javascript" defer/>
